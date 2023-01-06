22c522bc2c494ae38d0a0f7fdaf6f127942b3f8e
Pc1 <vicortobdel@gmail.com>
2023-01-06 13:26:54 +0700

    actual 05.01.2023

--- car/models.py
+++ car/models.py
@@ -7,3 +7,9 @@ class Client(models.Model):
     phone_Number_Regex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
     phone_number = models.CharField(validators=[phone_Number_Regex], max_length=11, unique=True)
 
+
+class Car(models.Model):
+    name_car = models.CharField(max_length=50)
+    transmission = models.CharField(max_length=50)
+    enginе = models.CharField(max_length=10)
+    year = models.CharField(max_length=4)
