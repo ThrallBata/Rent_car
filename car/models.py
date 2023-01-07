2c47ee067d0fecd2321c3895547521e929491bc4
Pc1 <vicortobdel@gmail.com>
2023-01-07 15:44:33 +0700

    add form + client in db

--- car/models.py
+++ car/models.py
@@ -8,9 +8,14 @@ class Cars(models.Model):
     enginе = models.CharField(max_length=10)
     year = models.CharField(max_length=4)
 
+    def __str__(self):
+        return self.name_car
+
 
 class Client(models.Model):
     name = models.CharField(max_length=50)
     phone_Number_Regex = RegexValidator(regex=r"^\+?1?\d{8,15}$")
-    phone_number = models.CharField(validators=[phone_Number_Regex], max_length=11, unique=True)
+    phone_number = models.CharField(validators=[phone_Number_Regex], max_length=12, unique=True)
     car = models.ForeignKey(Cars, on_delete=models.CASCADE)
+
+
--- car/views.py
+++ car/views.py
@@ -4,11 +4,29 @@ from django.shortcuts import render
 from django.views import generic
 
 from .models import Client
+from .forms import ClientForm
 
 
 def index(request):
-    return render(request, 'car/index.html')
+    error = ""
+    if request.method == 'POST':
+        filled_form = ClientForm(request.POST)
+        if filled_form.is_valid():
+            filled_form.save()
+
+        else:
+            error = "Некорректно заполняна форма!"
+
+    form = ClientForm()
+
+    data = {
+        'form': form,
+        'error': error
+    }
+    return render(request, 'car/index.html', data)
 
 
 def pageNotFound(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+
