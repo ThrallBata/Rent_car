83ab915cb126770da65339bd1b8ab43d47450be5
Pc1 <vicortobdel@gmail.com>
2023-01-09 17:04:49 +0700

    huyna, thinking about car-button -> form.car

--- car/static/car/scripts/script.js
+++ car/static/car/scripts/script.js
@@ -9,17 +9,6 @@ for (let i = 0; i < buttons.length; i++) {
     }
 }
 
-document.getElementById("price-action").onclick = function () {
-    if (document.getElementById("name").value === "") {
-        alert("Заполните поле имя!");
-    } else if (document.getElementById("phone").value === "") {
-        alert("Заполните поле телефон!");
-    } else if (document.getElementById("car").value === "") {
-        alert("Заполните поле автомобиль!");
-    } else {
-        alert("Спасибо за заявку, мы свяжемся с вами в ближайшее время!");
-    }
-}
 
 document.addEventListener("DOMContentLoaded", function () {
     let layer = document.querySelector('.price-image');
