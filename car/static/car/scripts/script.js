0d4c54ff4378f836e3acc3ea992d3cafe918d764
Pc1 <vicortobdel@gmail.com>
2023-01-19 19:27:52 +0700

    learning JS for alerts after click button_form

--- car/static/car/scripts/script.js
+++ car/static/car/scripts/script.js
@@ -21,3 +21,15 @@ document.addEventListener("DOMContentLoaded", function () {
         elem.style.backgroundPositionX = '0' + (0.15 * window.pageYOffset) + 'px';
     })
 });
+
+document.getElementById("price-action").onclick = function () {
+    if (document.getElementById("name").value === "") {
+        alert("Заполните поле имя!");
+    } else if (document.getElementById("phone").value === "") {
+        alert("Заполните поле телефон!");
+    } else if (document.getElementById("car").value === "") {
+        alert("Заполните поле автомобиль!");
+    } else {
+        alert("Спасибо за заявку, мы свяжемся с вами в ближайшее время!");
+    }
+}
